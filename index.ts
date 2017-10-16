ecd273eadd45ebd85a47bb30f3490fda6f6376a2
Alex Smith <donnytyler.com>
2017-10-16 22:51:49 +0200

    add bluebird compatibility

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
+
+
 export interface PromiseResult<R, T> {
     res?: R
     err?: T
 }
 
-export default async function protect<R, T>(fn: Promise<R>): Promise<PromiseResult<R, T>> {
-    const tuple: PromiseResult<R, T> = {}
-
-    await fn.then(val => tuple.res = val).catch(err => tuple.err = err)
-
-    return tuple
-}
+interface PromiseLike<R> {
+    then: (val: any) => PromiseLike<R>
+    catch: (err: any) => void
+  }
+  
+  export default async function protect<R, T>(fn: PromiseLike<R>): Promise<PromiseResult<R, T>> {
+      const tuple: PromiseResult<R, T> = {}
+  
+      await fn.then(val => tuple.res = val).catch(err => tuple.err = err)
+  
+      return tuple
+  }
