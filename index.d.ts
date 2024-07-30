742d6f47344ba7caea89ae963552f84510cce96d
PW <24858082+nihiluis@users.noreply.github.com>
2024-07-31 00:24:29 +0200

    Updating to ESM.

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export type Result<R, T extends Error> = [R | undefined, T | undefined]
-
-export default function protect<R, T extends Error>(fn: Promise<R>): Promise<Result<R, T>>
--- index.ts
+++ index.ts
@@ -1,56 +1,63 @@
-type Result<R, T extends Error> = [R | undefined, T | undefined]
+type Result<R, T extends Error> = [R | undefined, T | undefined];
 
 /**
  * Wraps errors from your Promise (or similar) object into a [Result] which you can later destructure,
  * avoiding try { } catch (e) { } boilerplate.
- * 
+ *
  * @param fn The promise which is to be wrapped.
  */
-export default async function protect<R, T extends Error>(fn: Promise<R>): Promise<Result<R, T>> {
-  const res: Result<R, T> = [undefined, undefined]
+export default async function protect<R, T extends Error>(
+  fn: Promise<R>
+): Promise<Result<R, T>> {
+  const res: Result<R, T> = [undefined, undefined];
 
   try {
-    await fn.then(val => res[0] = val).catch((err: T) => res[1] = err)
+    await fn.then((val) => (res[0] = val)).catch((err: T) => (res[1] = err));
   } catch (e) {
-    res[1] = e as T
+    res[1] = e as T;
   }
 
-  return res
+  return res;
 }
 
+export { protect };
+
 /**
- * Wraps errors from multiple Promise (or similar) objects into a Result array which you can later 
+ * Wraps errors from multiple Promise (or similar) objects into a Result array which you can later
  * iterate and then destructure, avoiding try { } catch (e) { } boilerplate.
- * 
+ *
  * @param fns The promises which are to be wrapped.
  */
-export async function protectAll<R, T extends Error>(fns: Promise<R>[]): Promise<Result<R, T>[]> {
-  const tuples: Result<R, T>[] = []
+export async function protectAll<R, T extends Error>(
+  fns: Promise<R>[]
+): Promise<Result<R, T>[]> {
+  const tuples: Result<R, T>[] = [];
 
   const newFns = fns.map(async (fn, i) => {
-    tuples[i] = [undefined, undefined]
+    tuples[i] = [undefined, undefined];
 
     try {
-      const val = await fn
-      return tuples[i]![0] = val
+      const val = await fn;
+      return (tuples[i]![0] = val);
+    } catch (err) {
+      return (tuples[i]![1] = err as T);
     }
-    catch (err) {
-      return tuples[i]![1] = err as T
-    }
-  })
+  });
 
-  await Promise.all(newFns)
+  await Promise.all(newFns);
 
-  return tuples
+  return tuples;
 }
 
 /**
- * Wraps protect into a function to be lazily evaluated. 
- * 
+ * Wraps protect into a function to be lazily evaluated.
+ *
  * This can for example be used combined with redux-saga (for React).
- * 
- * @param fn 
+ *
+ * @param fn
  */
-export function lazyProtect<R, T extends Error>(fn: Promise<R>): () => Promise<Result<R, T>> {
-  return async () => await protect<R, T>(fn)
+export function lazyProtect<R, T extends Error>(
+  fn: Promise<R>
+): () => Promise<Result<R, T>> {
+  return async () => await protect<R, T>(fn);
 }
